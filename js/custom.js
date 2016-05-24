1cdf9def721ea67cd5ec9aa0d6518e5b065ad456
JSA123 <jed.ahern@gmail.com>
2016-05-24 22:50:09 +1000

    Menu etc

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,20 @@
 $(function()
 {
-	// jQuery methods go here...
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
 });
